9af286be29c1cae3b623d96fc1bc3709e2d62892
Thomas Striegl <pldcanfly@gmail.com>
2021-02-09 20:20:27 +0100

    Cleanup store

---
+++ src/index.d.ts
@@ -0,0 +1,36 @@
+type ClassTable = {
+  [propName: number]: ClassItem;
+};
+
+type ClassItem = {
+  name: string;
+  icon: string;
+  speccs: SpeccTable;
+};
+
+type SpeccTable = {
+  1: SpeccItem;
+  2: SpeccItem;
+  3: SpeccItem;
+  4?: SpeccItem;
+};
+
+type SpeccItem = {
+  name: string;
+  role: 'damage' | 'heal' | 'tank';
+  picture: string;
+};
+
+type RaceTable = {
+  [propName: number]: RaceItem;
+};
+
+type RaceItem = {
+  name: string;
+  picture: RaceItemPicture;
+};
+
+type RaceItemPicture = {
+  male: string;
+  female: string;
+};
